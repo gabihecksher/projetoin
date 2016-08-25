4bcc00769c514558776cc260e2c26f4fd950c39f
Gabriela <gabriela.hecksher@gmail.com>
2016-08-25 19:53:43 +0000

    relacionando entidades

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,4 @@
 class Author < ApplicationRecord
+    has_many :books, dependent: :destroy
     validates :name, :lastname, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+    belongs_to :author
+    belongs_to :category
+    belongs_to :publisher 
     validates :name, :price, :category_id, :author_id, :publisher_id, presence: true
     validates :description, length: { maximum: 300, minimum: 10 }
     validates :available_quantity, numericality: true
